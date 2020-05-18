f79c002737b27ceb93ef29888a6f7b1016b5a1bc
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-05-18 16:16:47 +0800

    Invoke Internet Explorer

--- src/testcases/InvokeIE.java
+++ src/testcases/InvokeIE.java
@@ -1,13 +1,19 @@
 package testcases;
 
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class InvokeIE {
 
 	public static void main(String[] args) {
 		
-		InternetExplorerDriver driver = new InternetExplorerDriver();
+		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		capabilities.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
 		
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\hello\\Documents\\SELENIUM\\Browser_Executables\\IEDriverServer.exe");
+		InternetExplorerDriver driver = new InternetExplorerDriver();
+		driver.get("http://way2automation.com/");		
 
 	}
 
